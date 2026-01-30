32c1700873b43519b08835d29c8eeb1e88540017
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-01-31 02:16:33 +0530

    Time: 13 ms (51.75%) | Memory: 49 MB (72.00%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,51 +1,20 @@
 class Solution {
-    public class Pair implements Comparable<Pair> {
-        int node;
-        int time;
-        Pair(int node , int time){
-            this.node = node;
-            this.time = time;
-        }
-        public int compareTo(Pair other) {
-            return Integer.compare(this.time, other.time);
-        }   
-    }
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i=0; i<=n; i++) adj.add(new ArrayList<>());
-        for(int i=0; i<times.length; i++){
-            int u = times[i][0];
-            int v = times[i][1];
-            int t = times[i][2];
-            adj.get(u).add(new Pair(v , t));
-        }
-
         int[] ans = new int[n+1];
         Arrays.fill(ans , Integer.MAX_VALUE);
-        ans[0] = -221;
         ans[k] = 0;
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(k , 0));
-
-        while(pq.size()>0){
-            Pair top = pq.remove();
-            int node = top.node;
-            int time = top.time;
-            if(time > ans[node]) continue;
-            for(Pair p : adj.get(node)){
-                int TotalTime = p.time + time;
-                if(TotalTime < ans[p.node]){
-                    ans[p.node] = TotalTime;
-                    pq.add(new Pair(p.node , TotalTime));
-                }
+        for(int x=1; x<n; x++){
+            for(int i=0; i<times.length; i++){
+                int u = times[i][0] , v = times[i][1] , wt = times[i][2];
+                if(ans[u] == Integer.MAX_VALUE) continue;
+                if(ans[u] + wt < ans[v]) ans[v] = ans[u] + wt;
             }
         }
-        int max = -1;
-        for(int ele : ans){
-            max = Math.max(max , ele);
+        int max = 0;
+        for(int i=1; i<=n; i++){
+            if(ans[i] == Integer.MAX_VALUE) return -1;
+            max = Math.max(max , ans[i]);
         }
-        if(max == Integer.MAX_VALUE) return -1;
         return max;
     }
 }
