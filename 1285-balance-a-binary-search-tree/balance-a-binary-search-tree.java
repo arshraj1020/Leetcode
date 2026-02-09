b20e6074a95fac60fa7c973a7180b11d3d0e416d
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-02-10 02:24:23 +0530

    Time: 1 ms (100.00%) | Memory: 47.8 MB (97.12%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void In(ArrayList<TreeNode> arr , TreeNode root){
+        if(root== null) return;
+        In(arr , root.left);
+        arr.add(root);
+        In(arr , root.right);
+    }
+    public TreeNode Attach(ArrayList<TreeNode> arr , int inHi, int inLo){
+        if(inLo>inHi) return null;
+        int mid = (inHi + inLo)/2;
+        TreeNode temp = arr.get(mid);
+        temp.left = Attach(arr , mid-1, inLo);
+        temp.right = Attach(arr , inHi, mid+1);
+        return arr.get((inHi+inLo)/2);
+    }
+    public TreeNode balanceBST(TreeNode root) {
+        ArrayList<TreeNode> arr = new ArrayList<>();
+        In(arr , root);
+        int inHi = arr.size()-1;
+        int inLo = 0;
+        return Attach(arr , inHi, inLo);
+    }
+}
