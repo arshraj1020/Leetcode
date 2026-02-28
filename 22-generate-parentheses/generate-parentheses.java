8cb9b28fcacd1d4ce0cd61e83be8ae632e4dec76
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-03-01 00:08:08 +0530

    Time: 2 ms (68.11%) | Memory: 45.5 MB (7.58%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,16 +1,23 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    public void genrate(int n , String s, int nOpen , int nClose){
+    public void genrate(int n , String s, int nOpen , int nClose , List<String> ans){
         int m = s.length();
-        if(n*2 == m){
+        if(nOpen == n && nClose == n){
             ans.add(s);
             return;
         }
-        if(nOpen < n)genrate(n , s+"(" , nOpen+1 , nClose);
-        if(nOpen > nClose) genrate(n , s+")" , nOpen , nClose+1);
+        if(m == 0) genrate(n , s+'(' , nOpen+1 , nClose , ans);
+        else if(nOpen<n && nOpen>=nClose){
+            genrate(n , s+')' , nOpen , nClose+1 , ans);
+            genrate(n , s+'(' , nOpen+1 , nClose , ans);
+        }else if(nOpen == n){
+            genrate(n , s+')' , nOpen , nClose+1 , ans);
+        }
     }
     public List<String> generateParenthesis(int n) {
-        genrate(n , "" , 0 , 0);
+        int nOpen = 0;
+        int nClose = 0;
+        List<String> ans = new ArrayList<>();
+        genrate(n , "" , nOpen , nClose , ans);
         return ans;
     }
 }
