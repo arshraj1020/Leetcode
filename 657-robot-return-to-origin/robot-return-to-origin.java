3772bb3154a0f45ad3bf8f5dd410342950ee4237
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-04-05 18:00:31 +0530

    Time: 7 ms (30.80%) | Memory: 45.4 MB (91.72%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int[] ch = new int[26];
-        for (char move : moves.toCharArray()){
-            ch[move - 'A']++;
+        int[] arr = new int[4];
+        for(int i=0; i<moves.length(); i++){
+            char ch = moves.charAt(i);
+            if(ch == 'R') arr[0]++;
+            else if(ch == 'L') arr[1]++;
+            else if(ch == 'U') arr[2]++;
+            else if(ch == 'D') arr[3]++;
         }
-        return ch['U' - 'A'] == ch['D' - 'A'] && 
-                ch['L' - 'A'] == ch['R' - 'A'];
+        return (arr[0] == arr[1] && arr[2] == arr[3]);
     }
 }
