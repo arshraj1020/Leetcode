66df26e2521604e0dbf17eb8fa927df4d1bcf87f
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-04-05 17:59:16 +0530

    Time: 7 ms (30.80%) | Memory: 45.5 MB (84.38%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0; i<moves.length(); i++){
             char ch = moves.charAt(i);
             if(ch == 'R') arr[0]++;
-            if(ch == 'L') arr[1]++;
-            if(ch == 'U') arr[2]++;
-            if(ch == 'D') arr[3]++;
+            else if(ch == 'L') arr[1]++;
+            else if(ch == 'U') arr[2]++;
+            else if(ch == 'D') arr[3]++;
         }
         if(arr[0] == arr[1] && arr[2] == arr[3]) return true;
         return false;
