7f7f52b4bafec417ded2b9f143d54da3a616e618
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-04-17 01:30:36 +0530

    Time: 7 ms (95.38%) | Memory: 47.5 MB (30.34%) - LeetSync

---
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        List<int[]> list = new ArrayList<>();
+        for (int[] person : people) list.add(person[1], person);
+        return list.toArray(new int[list.size()][]);
+    }
+}
