37b96c99c133e62d314bd1e60223c4b211b27c7e
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-06-09 00:48:18 +0530

    Time: 12 ms (26.85%) | Memory: 151.2 MB (95.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         Queue<Integer> q = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
+        int count = 0;
         int idx = 0;
         int[] ans = new int[nums.length];
         for(int ele : nums){
             if(ele < pivot) ans[idx++] = ele;
-            else if(ele == pivot) q2.add(ele);
+            else if(ele == pivot) count++;
             else q.add(ele);
         }
-        while(q2.size() > 0) ans[idx++] = q2.remove();
+        for(int i=0; i<count; i++) ans[idx++] = pivot;
         while(q.size() > 0) ans[idx++] = q.remove();
         return ans;
     }
