ee0e2dda9e96c13b36842d93b965a8feb07a283e
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-08-20 12:28:28 +0530

    Time: 2 ms (57.45%) | Memory: 46.8 MB (42.79%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -2,25 +2,16 @@ class Solution {
     public int[] resultArray(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
+        ArrayList<Integer> arr1 = new ArrayList<>(); ArrayList<Integer> arr2 = new ArrayList<>();
+        arr1.add(nums[0]); arr2.add(nums[1]);
         for(int i= 2; i<n; i++){
-            if(arr1.get(arr1.size()-1) > arr2.get(arr2.size()-1)){
-                arr1.add(nums[i]);
-            }else {
-                arr2.add(nums[i]);
-            }
+            if(arr1.get(arr1.size()-1) > arr2.get(arr2.size()-1)) arr1.add(nums[i]);
+            else arr2.add(nums[i]);
         }
         int j=0;
-        while(j < arr1.size()){
-            ans[j] = arr1.get(j++);
-        }
+        while(j < arr1.size()) ans[j] = arr1.get(j++);
         int x=0;
-        while(x < arr2.size()){
-            ans[j++] = arr2.get(x++);
-        }
+        while(x < arr2.size()) ans[j++] = arr2.get(x++);
         return ans;
     }
 }
