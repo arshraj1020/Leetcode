223671d459c24c14d021ce8d64f21ff2d0e3fee6
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-08-20 12:27:36 +0530

    Time: 1 ms (98.08%) | Memory: 46.9 MB (23.80%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -7,9 +7,7 @@ class Solution {
         arr1.add(nums[0]);
         arr2.add(nums[1]);
         for(int i= 2; i<n; i++){
-            int s1 = arr1.size();
-            int s2 = arr2.size();
-            if(arr1.get(s1-1) > arr2.get(s2-1)){
+            if(arr1.get(arr1.size()-1) > arr2.get(arr2.size()-1)){
                 arr1.add(nums[i]);
             }else {
                 arr2.add(nums[i]);
