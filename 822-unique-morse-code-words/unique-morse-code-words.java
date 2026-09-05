e18e0be821b591ad034028074d2eac34a9061ec2
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-09-06 00:57:26 +0530

    Time: 3 ms (37.62%) | Memory: 43.2 MB (76.05%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.java
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -1,39 +1,13 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
-        HashMap<Character, String> mp = new HashMap<>();
-        mp.put('a', ".-");
-        mp.put('b', "-...");
-        mp.put('c', "-.-.");
-        mp.put('d', "-..");
-        mp.put('e', ".");
-        mp.put('f', "..-.");
-        mp.put('g', "--.");
-        mp.put('h', "....");
-        mp.put('i', "..");
-        mp.put('j', ".---");
-        mp.put('k', "-.-");
-        mp.put('l', ".-..");
-        mp.put('m', "--");
-        mp.put('n', "-.");
-        mp.put('o', "---");
-        mp.put('p', ".--.");
-        mp.put('q', "--.-");
-        mp.put('r', ".-.");
-        mp.put('s', "...");
-        mp.put('t', "-");
-        mp.put('u', "..-");
-        mp.put('v', "...-");
-        mp.put('w', ".--");
-        mp.put('x', "-..-");
-        mp.put('y', "-.--");
-        mp.put('z', "--..");
-
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
         HashSet<String> set = new HashSet<>();
         for(String s : words){
             StringBuilder curr = new StringBuilder();
             for(int i=0; i<s.length(); i++){
                 char ch = s.charAt(i);
-                curr.append(mp.get(ch));
+                int idx = ch-'a';
+                curr.append(morse[idx]);
             }
             set.add(curr.toString());
         }
