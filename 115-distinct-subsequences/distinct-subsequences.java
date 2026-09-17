6e4abba09fc2cb828e39f7433996b19068c53ca8
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-09-17 13:21:55 +0530

    Time: 26 ms (9.64%) | Memory: 57.7 MB (13.50%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,20 +1,16 @@
 class Solution {
-    public int count(String s, String t, int[][] dp, int i, int j){
+    Integer[][] dp;
+    public int count(String s, String t, int i, int j){
         if(j >= t.length()) return 1;
         if(i >= s.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != null) return dp[i][j];
         int take = 0;
-        if(s.charAt(i) == t.charAt(j)) take = count(s , t , dp , i+1 , j+1);
-        int skip = count(s , t , dp , i+1 , j);
+        if(s.charAt(i) == t.charAt(j)) take = count(s , t , i+1 , j+1);
+        int skip = count(s , t, i+1 , j);
         return dp[i][j] = take + skip;
     }
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()][t.length()];
-        for(int i=0; i<s.length(); i++){
-            for(int j=0; j<t.length(); j++){
-                dp[i][j] = -1;
-            }
-        }
-        return count(s , t , dp , 0 , 0);
+        dp= new Integer[s.length()][t.length()];
+        return count(s , t , 0 , 0);
     }
 }
