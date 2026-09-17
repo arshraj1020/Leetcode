477fb06d55a899a53ce73c39e83ce9112e8f3335
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-09-17 13:14:03 +0530

    Time: 26 ms (9.64%) | Memory: 54.4 MB (33.99%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,22 +1,20 @@
 class Solution {
-    private int count(int i, int j, String s, String t, int[][] dp){
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
+    public int count(String s, String t, int[][] dp, int i, int j){
+        if(j >= t.length()) return 1;
+        if(i >= s.length()) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int take = 0;
-        if(s.charAt(i) == t.charAt(j)) take = count(i+1 , j+1, s , t, dp);
-        int notTake = count(i+1 , j, s , t, dp);
-        return dp[i][j] = take + notTake;
+        if(s.charAt(i) == t.charAt(j)) take = count(s , t , dp , i+1 , j+1);
+        int skip = count(s , t , dp , i+1 , j);
+        return dp[i][j] = take + skip;
     }
     public int numDistinct(String s, String t) {
-        int n = s.length(), m = t.length();
-        int[][] dp = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        int[][] dp = new int[s.length()][t.length()];
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<t.length(); j++){
                 dp[i][j] = -1;
             }
         }
-        if(n<m) return 0;
-        return count(0 , 0, s , t, dp);
+        return count(s , t , dp , 0 , 0);
     }
 }
