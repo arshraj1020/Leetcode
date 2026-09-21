29362e472427aa48ad3cdb9ad09b44fe99497fff
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-09-21 23:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (90.48%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int i=0; i<arr1.length; i++){
-            if(map.containsKey(arr1[i])){
-                map.put(arr1[i] , map.get(arr1[i])+1); 
-            }else map.put(arr1[i] , 1);
-        }
-        int x = 0;
-        for(int i=0; i<arr2.length; i++){
-            int ele = arr2[i];
-            int freq = map.get(arr2[i]);
-            for(int j=1; j<=freq; j++){
-                arr1[x++] = ele;
-            }
-            map.remove(ele);
-        }
-        ArrayList<Integer> remaining = new ArrayList<>(map.keySet());
-        Collections.sort(remaining);
-        for (int ele : remaining) {
-            int freq = map.get(ele);
-            for (int j = 1; j <= freq; j++) {
-                arr1[x++] = ele;
+        int n = arr1.length;
+        int[] res = new int[n];
+        int[] freq = new int[1001];
+        for (int num : arr1) freq[num]++;
+        int k = 0;
+        for (int num : arr2) {
+            while (freq[num] > 0) {
+                res[k++] = num;
+                freq[num]--;
             }
         }
-        return arr1;
+        for (int i = 0; i <= 1000; i++) {
+            while (freq[i] > 0) {
+                res[k++] = i;
+                freq[i]--;
+            }
+        }
+        return res;
     }
 }
