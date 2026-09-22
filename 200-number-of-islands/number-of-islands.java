c5b299b6a3839c8516903f7f45e2ceae651fad4a
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-09-22 23:28:47 +0530

    Time: 10 ms (5.29%) | Memory: 51.5 MB (82.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,41 +1,40 @@
 class Solution {
-    public class Pair {
-        int x, y;
-        Pair(int x, int y) {
+    class Pair{
+        int x; int y;
+        Pair(int x , int y){
             this.x = x;
             this.y = y;
         }
     }
-
     public int numIslands(char[][] grid) {
-        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        int n = grid.length , m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
         Queue<Pair> q = new LinkedList<>();
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j] || grid[i][j] == '0') continue;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j] == true || grid[i][j] == '0') continue;
                 ans++;
-                q.add(new Pair(i, j));
+                q.add(new Pair(i , j));
                 vis[i][j] = true;
-                while (!q.isEmpty()) {
+                while(q.size() != 0){
                     Pair front = q.remove();
-                    int x = front.x, y = front.y;
-                    if (x > 0 && !vis[x-1][y] && grid[x-1][y] == '1') {
-                        vis[x-1][y] = true;
-                        q.add(new Pair(x-1, y));
-                    }
-                    if (x + 1 < n && !vis[x+1][y] && grid[x+1][y] == '1') {
-                        vis[x+1][y] = true;
-                        q.add(new Pair(x+1, y));
-                    }
-                    if (y > 0 && !vis[x][y-1] && grid[x][y-1] == '1') {
-                        vis[x][y-1] = true;
-                        q.add(new Pair(x, y-1));
-                    }
-                    if (y + 1 < m && !vis[x][y+1] && grid[x][y+1] == '1') {
+                    int x = front.x , y = front.y;
+                    if(y+1 < m && vis[x][y+1] != true && grid[x][y+1] == '1'){
                         vis[x][y+1] = true;
-                        q.add(new Pair(x, y+1));
+                        q.add(new Pair(x , y+1));
+                    }
+                    if(x+1 < n && vis[x+1][y] != true && grid[x+1][y] == '1'){
+                        vis[x+1][y] = true;
+                        q.add(new Pair(x+1 , y));
+                    }
+                    if(y-1 >= 0 && vis[x][y-1] != true && grid[x][y-1] == '1'){
+                        vis[x][y-1] = true;
+                        q.add(new Pair(x , y-1));
+                    }
+                    if(x-1 >= 0 && vis[x-1][y] != true && grid[x-1][y] == '1'){
+                        vis[x-1][y] = true;
+                        q.add(new Pair(x-1 , y));
                     }
                 }
             }
