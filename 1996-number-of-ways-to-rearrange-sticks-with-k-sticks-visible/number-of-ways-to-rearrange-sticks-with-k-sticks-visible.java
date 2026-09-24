e535ed05d2a3a8e2366039060dbc495f4bbcf823
Arsh Raj <160102767+arshraj1020@users.noreply.github.com>
2026-09-24 13:47:51 +0530

    Time: 47 ms (19.53%) | Memory: 58.4 MB (79.69%) - LeetSync

---
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
@@ -0,0 +1,19 @@
+class Solution {
+    int mod = 1000000000+7;
+    public int sub(int n , int k, int[][] dp){
+        if(n < k || (k == 0 && n != 0)) return 0;
+        if(n == 0 && k== 0) return 1;
+        if(dp[n][k] != -1) return dp[n][k];
+        long ans = (sub(n-1 , k-1, dp) + (long)(n-1) * sub(n-1 , k , dp)) % mod; 
+        return  dp[n][k] = (int)ans;
+    }
+    public int rearrangeSticks(int n, int k) {
+        int[][] dp = new int[n+1][k+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=k; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return sub(n , k, dp);
+    }
+}
